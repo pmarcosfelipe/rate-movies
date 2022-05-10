dba43ea4257b4fb41876b1a6a2420c1e2f0f3747
Marcos Felipe <p.marcosfelipe@gmail.com>
2022-05-10 09:02:49 +0100

    add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import Header from 'components/Header/index';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+
+import Header from 'components/Header';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+import './styles.css';
+
+const Listing = () => {
+  return <h1>Listing</h1>;
+};
+
+export default Listing;
