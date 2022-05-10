692c507be71ba9d220eea5e7f608ecedf07c5afb
Marcos Felipe <p.marcosfelipe@gmail.com>
2022-05-10 05:48:55 +0100

    setting up project

---
+++ backend/src/main/java/com/marcosfelipe/ratemovies/RatemoviesApplication.java
@@ -0,0 +1,13 @@
+package com.marcosfelipe.ratemovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatemoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatemoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/marcosfelipe/ratemovies/RatemoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosfelipe.ratemovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatemoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
