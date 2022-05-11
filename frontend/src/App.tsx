bd2ded9775089e366dbeb3cd7e3cf21293c08485
Marcos Felipe <p.marcosfelipe@gmail.com>
2022-05-11 17:46:06 +0100

    replacing fixed information by object data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
 
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -9,11 +9,16 @@ const Form = () => {
     count: 2,
     score: 4.5,
   };
+
   return (
     <div className="dsmovie-form-container">
-      <img className="dsmovie-movie-card-image" src="url" alt="The Witcher" />
+      <img
+        className="dsmovie-movie-card-image"
+        src={movie.image}
+        alt={movie.title}
+      />
       <div className="dsmovie-card-bottom-container">
-        <h3>"The Witcher"</h3>
+        <h3>{movie.title}</h3>
         <form className="dsmovie-form">
           <div className="form-group dsmovie-form-group">
             <label htmlFor="email">Informe seu email</label>
